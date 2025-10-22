1bd5b62f1ed2b902d806cc923f973804bc3ebbdd
XA-12777 <3175237429@qq.com>
2025-10-22 20:53:15 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [vue()],
   base: './', // 使用相对路径
   build: {
-    outDir: 'dist',
+    outDir: 'docs', // 改为 docs
     assetsDir: 'assets',
     rollupOptions: {
       output: {
